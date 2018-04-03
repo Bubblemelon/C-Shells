9a22863912c8acc16e42191a9cfa705e48953cc1
bubblemelon <c.f.2y.93@gmail.com>
2018-04-02 22:40:21 -0700

    need to figure out feeding input

--- Code Jam Practice/Scrambled Itinerary/ScrambledI.cpp
+++ Code Jam Practice/Scrambled Itinerary/ScrambledI.cpp
@@ -1,8 +1,21 @@
 #include <iostream> // to do cout and cin
-#include <fstream> // manipulate files
+#include <string> // allow for string type
 
 
 int main(int argc, char const *argv[]) {
+
+  int case_num;
+
+  int num_flights;
+
+  // take the number of cases from 1st line
+  std::cin >> case_num;
+
+  // for each case, starting with 1 up to case_num
+  for(int i = 1; case_num >= i; ++i)
+  {
+    std::cout << "Case #" << i << ": ";
+  }
 
   return 0;
 }
