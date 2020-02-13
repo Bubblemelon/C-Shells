83264b6c3a158453452828d8877866621733507e
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-02-13 10:45:01 -0800

    add loan.c introduction

---
+++ Misc/c-fundamentals/loan.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+/* A program that calculates the remaining balance of a loan after the 1st to
+ * the 5th monthly payments.
+ *
+ * This a comment-block, and the only comment notation for C89.
+ * "//" was introduced in C99.
+ */
