72c7545de1533447b8a79faa944ad7b7b70c7556
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-02-15 21:42:10 -0800

    add arrays.c description and function prototypes

---
+++ Misc/c-fundamentals/arrays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* Takes an int array and returns the sum of the 1st and 3rd element. */
+int sumIndex0and2(int array[]);
+
+/* Returns the sum of all elements
+in the subarray starting with index start until index end */
+int sumOfSubarray(int array[], int start, int end);
+
+/* Returns the largest subarray sum of elements */
+int largestSubarraySum(int array[], int array_size);
+
+/* Returns the largest sum from the elements in an array */
+int largestSum(int array[], int array_size);
+
