99a260a0ee44c040c14a27480ba9cbb02c3bb0d2
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-02-26 19:29:03 -0800

    add digit-sum.c implementation

--- Misc/c-fundamentals/digit-sum.c
+++ Misc/c-fundamentals/digit-sum.c
@@ -4,3 +4,22 @@ e.g.
 if the user input is 1234,
 then the sum is, 1234 = 1+2+3+4 = 10
  */
+#include <stdio.h>
+
+int main()
+{
+    long int input;
+    long int result;
+
+    printf("\n*** Sums the Digits in a Number ***\n");
+
+    printf("\nEnter a number: ");
+    scanf("%ld", &input);
+
+    while( input > 0){
+        result += (input % 10);
+        input /= 10;
+    }
+    printf("The sum of the digits is : %lu\n", result);
+    return 0;
+}
