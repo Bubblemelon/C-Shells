9b2728fcd072e1c729ca2e94dc2db1248d90164b
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-02-28 17:28:51 -0800

    add description of factorial.c

---
+++ Misc/c-fundamentals/factorial.c
@@ -0,0 +1,7 @@
+/* A program that takes an int and returns the factorial of the given number. */
+
+// 1, 1, 2, 6, 24, 120, 720, 5040
+// n! = n * (n -1) * (n - 2) ( n - 3) * ... * 3 * 2 * 1
+
+// Note:
+// 0! = 1
