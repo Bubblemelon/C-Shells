23a37fac63f46183617144c4bf06fddaa0f2224f
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-03-05 11:02:56 -0800

    implement the conversion logic from Fahrenheit to Celsius in main

--- Misc/c-fundamentals/fahrenheit-to-celsius.c
+++ Misc/c-fundamentals/fahrenheit-to-celsius.c
@@ -5,3 +5,40 @@
  *
  * (°F − 32) × 5/9 = 0°C
  */
+#define FREEZING_POINT 32.0f
+#define FACTOR (5.0f/9.0f)
+
+/*
+ * Avoid using integers for division e.g
+ * d = 5/9 = 0.55555555555
+ * if d was type int, then d = 0 !!!
+ *
+ * Integers truncate decimal values.
+ */
+
+
+int main()
+{
+    float fahrenheit;
+    int c;
+
+    printf("\n~~~ Fahrenheit to Celsius Converter ~~~\n");
+    printf("Enter temperature in fahrenheit: ");
+
+    // store the input at the memory location of variable f
+    scanf("%f", &fahrenheit);
+
+    float celsius = ((fahrenheit - FREEZING_POINT) * FACTOR);
+
+    printf("\nUsing 'floats' for calculation:\n");
+    printf("Temperature %.2fF is %.2fC.\n",
+            fahrenheit,
+            celsius
+           );
+
+    c = celsius;
+
+    printf("\nTemperature in celsius gets truncated when using 'integers': %d\n", c);
+
+    return 0;
+}
