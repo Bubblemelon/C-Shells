4ef89b7dbf6af68b8c412de7a5c9480be6727490
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-03-09 09:13:52 -0700

    add description of gcd.c

---
+++ Misc/c-fundamentals/gcd.c
@@ -0,0 +1,7 @@
+/*  Write a C program that takes two positive int and calculates their greatest common divisor, as specified in the example below:
+
+e.g.
+Enter the first number : 100
+Enter the second number : 150
+The greatest common divisor of 100 and 150 is 50.
+ */
