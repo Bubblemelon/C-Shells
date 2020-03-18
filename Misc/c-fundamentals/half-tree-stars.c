ed162a3b4dfe812a60275eaba63d23a01b7eeada
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-03-17 23:45:56 -0700

    add function prototypes

--- Misc/c-fundamentals/half-tree-stars.c
+++ Misc/c-fundamentals/half-tree-stars.c
@@ -12,3 +12,12 @@ This program prints half a christmas tree.
 *********
 
 */
+
+#include <stdio.h>
+
+// prints stars using two-level nested for-loops
+void printStar(int n);
+// print stars using recursion
+void printStarRecurse(int n, int start);
+// print stars using recursion (upside down)
+void printStarRecurseD(int n);
