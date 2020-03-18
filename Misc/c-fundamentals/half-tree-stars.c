a92cce34513d6150b05bbeee3e9f37650664975d
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-03-18 00:50:04 -0700

    implement printStar()

--- Misc/c-fundamentals/half-tree-stars.c
+++ Misc/c-fundamentals/half-tree-stars.c
@@ -21,3 +21,18 @@ void printStar(int n);
 void printStarRecurse(int n, int start);
 // print stars using recursion (upside down)
 void printStarRecurseD(int n);
+
+void printStar(int n){
+
+    int count = 0;
+
+    for(int i=0; i <= n; i += 1 )
+    {
+        count = i;
+        while(count > 0){
+            printf("*");
+            count--;
+        }
+        printf("\n");
+    }
+}
