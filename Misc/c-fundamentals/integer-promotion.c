be3335b254ee265e83cc2142838c6f927800a6bc
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-03-27 08:30:27 -0700

    add integer-promotion.c description

---
+++ Misc/c-fundamentals/integer-promotion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+ * Functions that mimic ctype.h functions, i.e. isdigit(), islower(), and isupper().
+ *
+ * See a list of ctype.h functions in "/usr/include/ctype.h"
+ * OR here,
+ * https://www.programiz.com/c-programming/library-function/ctype.h/isdigit
+ * */
