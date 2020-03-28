5698d0612025b8e31bd7baeaaf9a341090d5dcf7
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-03-28 12:03:51 -0700

    add function prototypes

--- Misc/c-fundamentals/integer-promotion.c
+++ Misc/c-fundamentals/integer-promotion.c
@@ -7,4 +7,8 @@
  * See a list of ctype.h functions in "/usr/include/ctype.h"
  * OR here,
  * https://www.programiz.com/c-programming/library-function/ctype.h/isdigit
- * */
+ */
+
+bool isDigit(char c); /*Returns true if c is a digit.*/
+bool isLower(char c); /*Returns true if c is an uppercase letter.*/
+bool isUpper(char c); /*Returns true if c is a lowercase letter.*/
