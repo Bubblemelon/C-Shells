8abd092fdf69531b5666eadaad28b7bfa352b520
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-04-08 15:06:27 -0700

    add include directives for max-values.c

---
+++ Misc/c-fundamentals/max-values.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+
+// https://docs.microsoft.com/en-us/cpp/c-language/cpp-integer-limits?view=vs-2019
+#include <limits.h>
