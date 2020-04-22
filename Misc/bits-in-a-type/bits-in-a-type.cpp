ee3dc8cb3a96c6d586874989690fd05f58026ddf
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-04-21 20:09:16 -0700

    add description for bits-in-a-type.cpp

---
+++ Misc/bits-in-a-type/bits-in-a-type.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //to use `cout`
+#include <climits>  //to use `CHAR_BITS`
+#include <stddef.h> //to use `size_t`
+
+//to use e.g. uint8_t, see http://www.cplusplus.com/reference/cstdint/ for full list
+#include <stdint.h>
+#include <cstdio> // to use printf
+#include <string> // for strings
+using namespace std;
+
+/*
+ * This program returns the number of bits in a type.
+ */
