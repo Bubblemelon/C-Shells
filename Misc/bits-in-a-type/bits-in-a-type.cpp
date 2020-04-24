fbdea81b31a86d9515ef55a336df4f6da6882e4d
bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2020-04-23 23:10:45 -0700

    add function protoypes

--- Misc/bits-in-a-type/bits-in-a-type.cpp
+++ Misc/bits-in-a-type/bits-in-a-type.cpp
@@ -11,3 +11,20 @@ using namespace std;
 /*
  * This program returns the number of bits in a type.
  */
+
+// constexpr: indicates expression is constant and if possible computed at compile time. Introduced in C++11.
+//
+// https://docs.microsoft.com/en-us/cpp/cpp/constexpr-cpp?view=vs-2019
+
+// returns the number of bits in a byte without using CHAR_BITS
+constexpr unsigned short bits_per_byte();
+
+// returns the number of bits in a byte "using" CHAR_BITS
+template <typename T>
+size_t bits_in_a_type();
+
+// prints the number of bytes in a type, takes string t for printing
+template <typename T>
+void printTypeNumBits(unsigned short bits, string t);
+
+////////////////////////////////////////////////////////////////////////////////
